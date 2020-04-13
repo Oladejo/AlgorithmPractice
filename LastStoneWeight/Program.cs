ef6ee187e90f3b3cdc25dfc79139b0053be6eb46
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-04-13 08:21:18 +0100

    add custom heap

--- LastStoneWeight/Program.cs
+++ LastStoneWeight/Program.cs
@@ -17,7 +17,8 @@ namespace LastStoneWeight
             Console.WriteLine($"Result: {LastStoneWeight(arr2)}");
 
             //Console.WriteLine($"Result: {LastStoneWeightHeap(arr)}");
-            Console.WriteLine($"Result: {LastStoneWeightHeap(arr2)}");
+            //Console.WriteLine($"Result: {LastStoneWeightHeap(arr2)}");
+            Console.WriteLine($"Result: {LastStoneWeightUsinfCustomHeap(arr2)}");
         }
 
         public static int LastStoneWeightHeap(int[] stones)
@@ -95,6 +96,74 @@ namespace LastStoneWeight
             }
 
             return heap.FirstOrDefault();
+        }
+
+        public static int LastStoneWeightUsinfCustomHeap(int[] stones)
+        {
+            BuildMaxHeap(stones);
+            while (stones.Length > 1)
+            {
+                int stone1 = ExtractMax(ref stones);
+                int stone2 = ExtractMax(ref stones);
+
+                if (stone1 - stone2 != 0)
+                    AddToMaxHeap(ref stones, (stone1 - stone2));
+            }
+
+            if (stones.Length == 0)
+                return 0;
+            else
+                return ExtractMax(ref stones);
+        }
+
+        public static void MaxHeapify(int[] A, int i) //Assuming array A is 0 based (not 1 based)
+        {
+            int largest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+            if (l < A.Length && A[l] > A[largest])
+                largest = l;
+            if (r < A.Length && A[r] > A[largest])
+                largest = r;
+            if (largest != i)
+            {
+                int temp = A[i];
+                A[i] = A[largest];
+                A[largest] = temp;
+                MaxHeapify(A, largest);
+            }
+        }
+
+        public static void BuildMaxHeap(int[] A)
+        {
+            for (int i = A.Length / 2; i >= 0; i--)
+            {
+                MaxHeapify(A, i);
+            }
+        }
+
+        public static int ExtractMax(ref int[] A)
+        {
+            if (A.Length == 1)
+            {
+                int retVal = A[0];
+                A = new int[0];
+                return retVal;
+            }
+
+            int root = A[0];
+            A[0] = A[A.Length - 1];
+            A = A.Take(A.Length - 1).ToArray();
+            MaxHeapify(A, 0);
+            return root;
+
+        }
+
+        public static void AddToMaxHeap(ref int[] A, int num)
+        {
+            Array.Resize(ref A, A.Length + 1);
+            A[A.Length - 1] = num;
+            BuildMaxHeap(A);
         }
     }
 }
